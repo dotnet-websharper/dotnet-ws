8a7823c100d6b4772754c05a9ea7083fc12563d7
jooseppi12 <urijozsef@europe.com>
2021-09-28 18:53:56 +0200

    Change versioning to follow x.y.z format

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 IF "%VERSION_PATCH%"=="" (
-    SET VERSION=0.1.0.0-preview1
+    SET VERSION=0.1.1-preview1
 ) ELSE (
-    SET VERSION=0.1.0.%VERSION_PATCH%-preview1
+    SET VERSION=0.1.%VERSION_PATCH%-preview1
 )
 
 echo %VERSION%
