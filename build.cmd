9637d837d01c962452a54600e2aa54af68f160c4
jooseppi12 <urijozsef@europe.com>
2023-08-08 09:58:39 +0200

    Add tagging functionality

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 IF "%VERSION_PATCH%"=="" (
-    SET VERSION=0.99994
+    SET VERSION=3.111.9
 ) ELSE (
     SET VERSION=0.1.%VERSION_PATCH%
 )
@@ -7,3 +7,11 @@ IF "%VERSION_PATCH%"=="" (
 echo %VERSION%
 
 dotnet pack dotnet-ws.fsproj -o publish -c Release /p:Version=%VERSION%
+
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+git tag %VERSION%
+
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+git push origin --tags
